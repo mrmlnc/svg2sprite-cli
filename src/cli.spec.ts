fcad359197d12ecdde5cf16b4c1d9f9b5624dfe0
Michel Hervé NGOUNOU <dev.ngounou@gmail.com>
2019-05-31 17:11:35 +0200

    add `deepId` feature

--- src/cli.spec.ts
+++ src/cli.spec.ts
@@ -15,6 +15,7 @@ describe('CLI', () => {
 			'.tmp/sprite.svg',
 			'--ignore=sprite.svg',
 			'--inline',
+			'--deepId',
 			'--stripExtraAttrs',
 			'--stripTags=title',
 			'--stripTags=desc',
@@ -22,7 +23,7 @@ describe('CLI', () => {
 			'--stripEmptyTags=g'
 		]).stdout;
 
-		assert.ok(stdout.indexOf('Created 1 SVG sprite from 2 SVG\'s.') !== -1);
+		assert.ok(stdout.indexOf('Created 1 SVG sprite from 3 SVG\'s.') !== -1);
 
 		const sprite = fs.readFileSync('.tmp/sprite.svg', 'utf-8');
 		assert.equal(sprite, svg.replace(/[\r\n\t]|\s{2,}/g, ''));
